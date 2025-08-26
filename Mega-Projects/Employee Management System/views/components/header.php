a2cadb7fc81894c70a82f52ae96638b84eebed4a
Tamal Pakrasi <tamalpakrasi11@gmail.com>
2025-08-26 21:53:43 +0530

    Add Heading of page and beginning of the responsive layout

---
+++ Mega-Projects/Employee Management System/views/components/header.php
@@ -0,0 +1,8 @@
+<header class="mt-8.5 ps-5">
+  <h2 class="text-white text-3xl font-bold mb-3">
+    <?php echo $active_page === "tasks" ? "All Tasks" : "All Employees" ?>
+  </h2>
+  <p class="text-gray-400 text-xs sm:text-sm text-justify leading-4 lg:leading-5">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quae dignissimos tenetur maxime, quas ducimus a maiores. Corrupti, quia quibusdam? Soluta dolor temporibus doloremque architecto!
+  </p>
+</header>
--- Mega-Projects/Employee Management System/views/partials/root.php
+++ Mega-Projects/Employee Management System/views/partials/root.php
@@ -1,9 +1,12 @@
 <div
   id="root"
-  class="bg-root min-h-[100dvh] py-15 px-5 sm:px-10 md:px-12 lg:px-15 flex flex-col relative">
-  <?php
-  include_once __DIR__ . "/../components/navbar.php";
-  if (str_contains($title, "Management")) {
-  }
-  ?>
+  class="bg-root min-h-[100dvh] py-15 px-5 flex flex-col relative">
+  <div class="container-max mx-auto">
+    <?php
+    include_once __DIR__ . "/../components/navbar.php";
+    include_once __DIR__ . "/../components/header.php";
+    if (str_contains($title, "Management")) {
+    }
+    ?>
+  </div>
 </div>
