bd873a88422d1d447d282a45520b9c9b73fad501
Tamal Pakrasi <tamalpakrasi11@gmail.com>
2025-08-27 16:40:58 +0530

    employee management: add responsive grid form layout

---
+++ Mega-Projects/Employee Management System/views/components/dropdown.php
@@ -0,0 +1,7 @@
+<div class="">
+  <label for="<?php echo $dropdown; ?>" class="block text-gray-400 mb-2 text-[12px] md:text-sm"><?php echo ucwords($dropdown); ?></label>
+
+  <select name="<?php echo $dropdown; ?>" id="<?php echo $dropdown; ?>" class="block w-full form-field font-semibold p-2">
+    <option selected disabled>-- Choose One --</option>
+  </select>
+</div>
--- Mega-Projects/Employee Management System/views/components/task_grid.php
+++ Mega-Projects/Employee Management System/views/components/task_grid.php
@@ -1,1 +1,22 @@
-<div>Hello</div>
+<?php
+$dropdowns = ["employee", "priority", "due date"];
+?>
+<form action="" method="post" class="form-grid">
+  <?php
+  foreach ($dropdowns as $dropdown) {
+    include __DIR__ . "/dropdown.php";
+  }
+  ?>
+
+  <div class="md:col-span-full lg:row-start-2 lg:col-span-full">
+    <label for="task_title" class="block text-gray-400 mb-2 text-[12px] md:text-sm">Task Title</label>
+    <input type="text" name="task_title" id="task_title" class="block w-full form-field p-3 border border-gray-700" placeholder="e.g., Design landing page">
+  </div>
+
+  <div class="md:col-span-full lg:row-start-3 lg:col-span-full">
+    <label for="task_desc" class="block text-gray-400 mb-2 text-[12px] md:text-sm">Description (optional)</label>
+    <textarea name="task_desc" id="task_desc" class="block w-full form-field p-3 border border-gray-700 resize-none" rows="3" placeholder="short note about the task"></textarea>
+  </div>
+
+  <input type="submit" value="Add Task" class="btn lg:row-start-1 lg:col-start-4 lg:self-end">
+</form>
