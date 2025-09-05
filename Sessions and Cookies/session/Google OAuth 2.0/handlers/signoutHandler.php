a3911e6492d4a06009c6fd192a80321fabbab246
Tamal Pakrasi <tamalpakrasi11@gmail.com>
2025-09-05 16:09:34 +0530

    google oAuth 2.0: Modify sign out to unset and destroy old session from the server but cookie stays  in the browser

--- Sessions and Cookies/session/Google OAuth 2.0/handlers/signoutHandler.php
+++ Sessions and Cookies/session/Google OAuth 2.0/handlers/signoutHandler.php
@@ -5,6 +5,9 @@ require_once __DIR__ . "/message.php";
 session_start();
 if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_SESSION["username"]) && isset($_SESSION["email"])) {
   session_unset();
+  session_destroy();
+
+  session_start();
   set_message("Signed out successfully");
   header("Location: ../pages/signin.php");
   exit;
