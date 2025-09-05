1bd6260f7201a390d695e0ff9178bac209769dba
Tamal Pakrasi <tamalpakrasi11@gmail.com>
2025-09-05 15:50:18 +0530

    google oAuth 2.0- Modify sign in user functionality

--- Sessions and Cookies/session/Google OAuth 2.0/utils/signInUser.php
+++ Sessions and Cookies/session/Google OAuth 2.0/utils/signInUser.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/../handlers/message.php";
 
 function signInUser(string $username, string $email): void
 {
-  session_status() !== PHP_SESSION_ACTIVE ? session_start() : session_regenerate_id();
+  session_regenerate_id(true);
   $_SESSION["username"] = $username;
   $_SESSION["email"] = $email;
   set_message("Signed in successfully");
